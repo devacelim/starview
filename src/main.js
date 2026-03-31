c517f0ba725d3f131835cd56e08278d85b032a84
acelim <acelim.hj@gmail.com>
2026-03-31 17:33:02 +0900

    modify version

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * main.js — StarView app entry point
  */
 
-const APP_VERSION = 'v2.1';
+const APP_VERSION = 'v2.2';
 
 import { loadSkyData, renderSky, hitTest, getStarsData, getConstsData } from './skymap.js';
 import { updateMoonScreen } from './moon.js';
